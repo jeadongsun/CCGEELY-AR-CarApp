1d53b2e39fe67db68cad9251b07fca2bd607655a
jeadongsun <38779851+jeadongsun@users.noreply.github.com>
2021-06-10 18:03:20 +0800

    # 添加ar扫车 界面

---
+++ Assets/ProJect/Script/TrackMenuContrl.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+public class TrackMenuContrl : MonoBehaviour
+{
+    public GameObject MessageBg1;
+
+    public GameObject MessageBg2;
+
+    public GameObject MessageBg3;
+
+    public GameObject MessageBg4;
+
+    public GameObject TestModel;
+
+    private void Start()
+    {
+        MessageBg1.GetComponent<RectTransform>().DOSizeDelta(new Vector2(4, 0), 0);
+        MessageBg1.GetComponent<RectTransform>().DOAnchorPos(new Vector2(-951, 10), 0);
+
+
+        MessageBg2.transform.DOLocalMoveX(-1176,0,false).SetEase(Ease.Linear);
+
+        MessageBg3.transform.DOLocalMoveX(-1216, 0, false).SetEase(Ease.Linear);
+
+        MessageBg4.transform.DOLocalMoveY(-498, 0, false).SetEase(Ease.Linear);
+
+        TestModel.SetActive(false);
+    }
+
+
+
+    public void OpenUI()
+    {
+        MessageBg1.GetComponent<RectTransform>().DOSizeDelta(new Vector2(4, 680), 0.5f);
+        MessageBg1.GetComponent<RectTransform>().DOAnchorPos(new Vector2(-951, 10), 0.5f);
+
+        MessageBg2.transform.DOLocalMoveX(-731, 0.5f, false).SetEase(Ease.InOutQuad).SetDelay(0.5f);
+
+        MessageBg3.transform.DOLocalMoveX(-498, 0.5f, false).SetEase(Ease.InOutQuad).SetDelay(1f);
+
+        MessageBg4.transform.DOLocalMoveY(-0, 0.5f, false).SetEase(Ease.InOutQuad).SetDelay(1f).OnComplete(() =>
+        {
+            TestModel.SetActive(true);
+        });
+    }
+
+    public void CloseUI()
+    {
+        TestModel.SetActive(false);
+
+        MessageBg4.transform.DOLocalMoveY(-498, 0.5f, false).SetEase(Ease.InOutQuad);
+
+        MessageBg3.transform.DOLocalMoveX(-1216, 0.5f, false).SetEase(Ease.InOutQuad);
+
+        MessageBg2.transform.DOLocalMoveX(-1176, 0.5f, false).SetEase(Ease.InOutQuad).SetDelay(0.5f);
+
+        MessageBg1.GetComponent<RectTransform>().DOSizeDelta(new Vector2(4, 0), 0.5f).SetDelay(1f) ;
+        MessageBg1.GetComponent<RectTransform>().DOAnchorPos(new Vector2(-951, 10), 0.5f).SetDelay(1f) ;
+    }
+}
