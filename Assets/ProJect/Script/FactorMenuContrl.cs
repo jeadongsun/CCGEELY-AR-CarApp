b5eec0d7b27fc31d7cc4be3fb25d63709b41fcc0
jeadongsun <38779851+jeadongsun@users.noreply.github.com>
2021-06-15 14:49:57 +0800

    #update

--- Assets/ProJect/Script/FactorMenuContrl.cs
+++ Assets/ProJect/Script/FactorMenuContrl.cs
@@ -7,25 +7,26 @@ using UnityEngine.Video;
 public class FactorMenuContrl : MonoBehaviour
 {
 
-    public GameObject VideoMenu;
+    public GameObject ChongYaBtnT;
 
-    public VideoPlayer testChongYaVideo;
+    public GameObject HanJieBtnT;
 
-    //冲压动画
-    public void ChongYaBtn()
+    public GameObject ZongZhuangBtn;
+
+    public GameObject TuZhuangBtn;
+
+    public GameObject CameraModel;
+
+    private void Update()
     {
-        VideoMenu.SetActive(true);
-        testChongYaVideo.Stop();
-        testChongYaVideo.Play();
+        ChongYaBtnT.transform.rotation = Quaternion.LookRotation(ChongYaBtnT.transform.position - CameraModel.transform.position);
+        HanJieBtnT.transform.rotation = Quaternion.LookRotation(HanJieBtnT.transform.position - CameraModel.transform.position);
+        ZongZhuangBtn.transform.rotation = Quaternion.LookRotation(ZongZhuangBtn.transform.position - CameraModel.transform.position);
+        TuZhuangBtn.transform.rotation = Quaternion.LookRotation(TuZhuangBtn.transform.position - CameraModel.transform.position);
     }
 
 
-    //关闭动画 界面
-    public void CloseVideoPlay()
-    {
-        testChongYaVideo.Stop();
-        VideoMenu.SetActive(false);
-    }
+
 
     //返回主场景
     public void BackMainMenu()
